d6eaf0011e79649abc9f5ad832180982dee58523
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-05-29 12:52:23 +0700

    Small fixes, removed unnecessaries

--- AutoLogin.js
+++ AutoLogin.js
@@ -1,17 +1,20 @@
 // ==UserScript==
 // @name         SSO UI, EMAS2, SCELE, and SIAK-NG AutoLogin
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  This userscript is made to automatically login to SCELE UI website using your account
+// @version      2.3
+// @description  This userscript is made to automatically login to UI websites using your account credentials. Set your username and password in the script before using it.
 // @author       absolutepraya
 // @match        https://scele.cs.ui.ac.id/
 // @match        https://scele.cs.ui.ac.id/login/index.php
 // @match        https://emas2.ui.ac.id/login/index.php
 // @match        https://academic.ui.ac.id/main/Authentication/
-// @match        https://sso.ui.ac.id/cas/login
+// @match        https://sso.ui.ac.id/cas/login*
+// @icon         https://i.ibb.co.com/m8vqKV2/favicon-32x32.png
+// @license      MIT
 // ==/UserScript==
 
 (function() {
+
     // replace ___username___ and ___password___ with your own username and password
     var username = "___username___";
     var password = "___password___";
@@ -23,8 +26,7 @@
     if (window.location.href === "https://academic.ui.ac.id/main/Authentication/") {
         inputName = document.getElementsByName("u")[0];
         inputPassword = document.getElementsByName("p")[0];
-        
-    } else if (window.location.href === "https://sso.ui.ac.id/cas/login") {
+    } else if (window.location.href.startsWith("https://sso.ui.ac.id/cas/login")) {
         inputName = document.getElementById("username");
         inputPassword = document.getElementById("password");
     } else {
@@ -39,10 +41,7 @@
     inputPassword.value = password;
 
     // click the login button
-    if (window.location.href === "https://sso.ui.ac.id/cas/login") {
-    submitButton = document.getElementsByClassName("btn btn-lg btn-primary")[0];
-    } else {
     submitButton = document.querySelector("[type='submit']");
-    }
     submitButton.click();
+
 })();
--- README.md
+++ README.md
@@ -7,9 +7,9 @@ This is a simple JavaSript to automate login to SSO UI, SCELE, EMAS2, and SIAK-N
 1. Install Tampermonkey extension on your browser.  
 [Chromium (Chrome, Edge)](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) | [Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) | [Safari](https://apps.apple.com/us/app/tampermonkey/id1482490089) | [Opera](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)
 
-2. Open the Tampermonkey dashboard and press the \[+\] button to create a new script.
-
-3. Copy the script from `ALL_AutoLogin.js` if you want to use all four, and paste it on Tampermonkey new script. Or copy the script from the individual file and paste it to each Tampermonkey script if you only want to use one or two.
+2. Install via Greasy Fork:  
+https://greasyfork.org/en/scripts/496434-sso-ui-emas2-scele-and-siak-ng-autologin  
+(or you might as well copy the script from `AutoLogin.js` and paste it to Tampermonkey new script)
 
 4. Change the `___username___` and `___password___` variable to your own username and password.
 
